06ad1dee8df06d358bd64f1c145bf9243fce2885
tgyhuj107 <107021045@gm.asia.edu.tw>
2018-09-26 15:44:43 +0800

    3.44

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,27 @@ public class Main {
         Scanner scn = new Scanner(System.in);
 //        int str = scn.next().charAt(0);
 //        System.out.println(Integer.toHexString(str));
-        String str1 = scn.next();
-        int a=Integer.parseInt(str1,16);
-        System.out.println((char)a);
+
+//        String str1 = scn.next();
+//        int a=Integer.parseInt(str1,16);
+//        System.out.println((char)a);
+
+//        int t = scn.nextInt();
+//        int s = scn.nextInt();
+//        if (s==1){
+//            System.out.println(Math.round((t-80)*0.7f*10)/10f);
+//        }else if (s==2){
+//            System.out.println(Math.round((float) (t-70)*0.6f*10)/10f);
+//        }
+
+//        int x = scn.nextInt();
+//        int y = scn.nextInt();
+//        float x1 =(float) Math.sqrt((x*x)+(y*y));
+//        if (x1<=100){
+//            System.out.println("IN");
+//        }else {
+//            System.out.println("OUT");
+//        }
 
 
     }
