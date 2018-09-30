275db603c95984c1d462c14851aff3582a0343cf
tgyhuj107 <40759247+tgyhuj107@users.noreply.github.com>
2018-09-30 21:10:28 +0800

    wtier 01~06

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,47 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+        //01英里轉公里
+//        Scanner scn = new Scanner(System.in);
+//        float mile =scn.nextFloat();
+//        float km =mile*1.6f;
+//        System.out.println(Math.round(km*100)/100f);
+        //02攝氏轉華氏
+//        Scanner scn =new Scanner(System.in);
+//        float c =scn.nextFloat();
+//        float f = (c*9/5f)+32;
+//        System.out.println(f);
+        //03合 差 乘
+//        Scanner scn =new Scanner(System.in);
+//        int a =scn.nextInt();
+//        int b =scn.nextInt();
+//        System.out.println(a+b);
+//        System.out.println(a-b);
+//        System.out.println(a*b);
+        //04總分 平均
+//        Scanner scn =new Scanner(System.in);
+//        int ch =scn.nextInt();
+//        int en =scn.nextInt();
+//        int ma =scn.nextInt();
+//        System.out.println(ch+en+ma);
+//        System.out.println((ch+en+ma)/3);
+        //05身高 體重
+//        Scanner scn =new Scanner(System.in);
+//        int cm =scn.nextInt();
+//        int kg =scn.nextInt();
+//        double a=cm/2.54f;
+//        double b=kg/0.454f;
+//        System.out.println(a);
+//        System.out.println(b);
+        //06公升轉加崙
+//        Scanner scn =new Scanner(System.in);
+//        float l =scn.nextFloat();
+//        float a = l*0.26418f;
+//        System.out.println(Math.round(a*10)/10f);
+
+
+
+
 //        int str = scn.next().charAt(0);
 //        System.out.println(Integer.toHexString(str));
 
