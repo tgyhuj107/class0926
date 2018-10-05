ae474f0e6ba8b10cba9d4dcd5f5e76445c7f4ebd
tgyhuj107 <40759247+tgyhuj107@users.noreply.github.com>
2018-10-06 00:30:30 +0800

    10/6  ~21

--- src/Main.java
+++ src/Main.java
@@ -93,7 +93,27 @@ public class Main {
 //        }else {
 //            System.out.println(Math.round((t2-70)*0.6f*10)/10f);
 //        }
-
+        //15零錢問題
+//        Scanner scn =new Scanner(System.in);
+//        int n=scn.nextInt(),a1=scn.nextInt(),a2=scn.nextInt(),a3=scn.nextInt();
+//        if (a1*15+a2*20+a3*30<=n){
+//            int d=n-(a1*15+a2*20+a3*30);
+//            int d50=d/50 , d5=d%50/5 , d1=d%5;
+//            System.out.println(d1);
+//            System.out.println(d5);
+//            System.out.println(d50);
+//        }else {
+//            System.out.println(0);
+//        }
+        //16判斷座標是否在範圍內
+//        Scanner scn =new Scanner(System.in);
+//        int x=scn.nextInt(), y=scn.nextInt();
+//        double x1=Math.sqrt((x*x)+(y*y));
+//        if (x1<=100){
+//            System.out.println("inside");
+//        }else {
+//            System.out.println("outside");
+//        }
         //17停車問題
 //        Scanner scn =new Scanner(System.in);
 //        int h1=scn.nextInt(),m1=scn.nextInt(),h2=scn.nextInt(),m2=scn.nextInt();
@@ -106,30 +126,82 @@ public class Main {
 //        }else{
 //            System.out.println((min30-8)*60+280);
 //        }
-
-//        int str = scn.next().charAt(0);
-//        System.out.println(Integer.toHexString(str));
-
-//        String str1 = scn.next();
-//        int a=Integer.parseInt(str1,16);
-//        System.out.println((char)a);
-
-//        int t = scn.nextInt();
-//        int s = scn.nextInt();
-//        if (s==1){
-//            System.out.println(Math.round((t-80)*0.7f*10)/10f);
-//        }else if (s==2){
-//            System.out.println(Math.round((float) (t-70)*0.6f*10)/10f);
-//        }
-
-//        int x = scn.nextInt();
-//        int y = scn.nextInt();
-//        float x1 =(float) Math.sqrt((x*x)+(y*y));
-//        if (x1<=100){
-//            System.out.println("IN");
+        //18季節判定
+//        Scanner scn =new Scanner(System.in);
+//        int month = scn.nextInt();
+//        if (month>=3 && month<=5){
+//            System.out.println("Spring");
+//        }else if (month>=6 && month<=8){
+//            System.out.println("Summer");
+//        }else if (month>=9 && month<=11){
+//            System.out.println("Autumn");
 //        }else {
-//            System.out.println("OUT");
+//            System.out.println("Winter");
 //        }
+        //19星座
+//        Scanner scn =new Scanner(System.in);
+//        int month =scn.nextInt() , data=scn.nextInt();
+//        if (month==2 && data<=18 || month==1&&data>=21){
+//            System.out.println("Aquarius");
+//        }else if (month==3&&data<=20 || month==2){
+//            System.out.println("Pisces");
+//        }else if (month==4&&data<=20 || month==3){
+//            System.out.println("Aries");
+//        }else if (month==5&&data<=21 || month==4){
+//            System.out.println("Taurus");
+//        }else if (month==6&&data<=21 || month==5){
+//            System.out.println("Gemini");
+//        }else if (month==7&&data<=22 || month==6){
+//            System.out.println("Cancer");
+//        }else if (month==8&&data<=23 || month==7){
+//            System.out.println("Leo");
+//        }else if (month==9&&data<=23 || month==8){
+//            System.out.println("Virgo");
+//        }else if (month==10&&data<=23 || month==9){
+//            System.out.println("Libra");
+//        }else if (month==11&&data<=22 || month==10){
+//            System.out.println("Scorpio");
+//        }else if (month==12&&data<=21 || month==11){
+//            System.out.println("Sagittarius");
+//        }else if (month==1 || month==12){
+//            System.out.println("Capricorn");
+//        }
+        //20閏年
+//        Scanner scn =new Scanner(System.in);
+//        int y =scn.nextInt();
+//        if (y%400==0){
+//            System.out.println("Bissextile Year");
+//        }else if (y%100==0){
+//            System.out.println("Common Year");
+//        }else if (y%4==0){
+//            System.out.println("Bissextile Year");
+//        }else {
+//            System.out.println("Common Year");
+//        }
+        //21電費
+        Scanner scn =new Scanner(System.in);
+        int w=scn.nextInt();
+        float v1,v2;
+        if (w>=120) {
+            v1=w*2.10f;
+            v2=w*2.10f;
+            w-=120;
+            if (w<=210){
+                System.out.println(w*3.02f+v1);
+                System.out.println(w*2.68f+v2);
+            }
+//            v1=v1+w*3.02f;
+//            v2=v1+w*2.68f;
+//            w-=210;
+
+        }else {
+            System.out.println(w*2.10f);
+            System.out.println(w*2.10f);
+        }
+
+
+
+
 
 
     }
